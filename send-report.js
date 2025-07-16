c8eb4321182f0587354b48ca2b34b94db9f289e5
VIDYA <vidya.s@codelynks.com>
2025-07-16 11:28:03 +0530

    updates send-report.js

--- send-report.js
+++ send-report.js
@@ -16,8 +16,10 @@ async function sendEmailReport() {
     // Set up email transporter
     const transporter = nodemailer.createTransport({
         service: 'gmail',
+    auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
+    },
     });
 
     // Mail options
